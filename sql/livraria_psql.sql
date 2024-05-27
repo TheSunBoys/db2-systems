df1df463aed6e270a2942f3f700ecc392854f34d
gabriel <batistutag190@gmail.com>
2024-05-27 14:56:25 -0300

    atualizado código sql da livraria com novas procedures e nova estrutura de pastas feita

--- sql/livraria_psql.sql
+++ sql/livraria_psql.sql
@@ -9,9 +9,20 @@ create table livros (
 	disponivel boolean default true
 );
 
-insert into livros (titulo, autor, ano_publicado) values ('Jurassic Park','Michael Crichton',1990);
-insert into livros (titulo, autor, ano_publicado) values ('Lost World','Michael Crichton',1995);
-insert into livros (titulo, autor, ano_publicado) values ('Frankenstein','Mary Shelly',1818);
+create or replace procedure cadastrar_livro (
+  title text,
+  autor_livro text,
+  ano int)
+language plpgsql
+as $$
+begin
+  insert into livros (titulo, autor, ano_publicado) values (title,autor_livro,ano);
+end;$$;
+
+-- cadastro de exemplos de livros 
+call cadastrar_livro('Jurassic Park','Michael Crichton',1990);
+call cadastrar_livro('Lost World','Michael Crichton',1995);
+call cadastrar_livro('Frankenstein','Mary Shelly',1818);
 
 create table emprestimos (
 	id_emprestimo SERIAL primary key,
@@ -43,7 +54,36 @@ end;$$;
 
 -- Chamando a procedure atualizar_ano_livro
 call atualizar_ano_livro(1, 2015);
-select * from livros where bookId = 1;
+
+-- procedure de consulta de livro
+create or replace procedure consultar_livro (
+  id_livro integer,
+  out bookId integer,
+  out titulo text,
+  out autor text,
+  out ano_publicado int,
+  out disponivel boolean)
+language plpgsql
+as $$
+begin
+  select l.bookId, l.titulo, l.autor, l.ano_publicado, l.disponivel
+  into bookId, titulo, autor, ano_publicado, disponivel
+  from livros l
+  where l.bookId = id_livro;
+end;$$;
+
+-- chamada de consultar_livro
+do $$
+declare
+  v_bookId integer;
+  v_titulo text;
+  v_autor text;
+  v_ano_publicado int;
+  v_disponivel boolean;
+begin
+  call consultar_livro(1, v_bookId, v_titulo, v_autor, v_ano_publicado, v_disponivel);
+  raise notice 'Book ID: %, Title: %, Author: %, Year: %, Available: %', v_bookId, v_titulo, v_autor, v_ano_publicado, v_disponivel;
+end;$$;
 
 -- procedure de remoção de livros
 create or replace procedure remover_livro (
